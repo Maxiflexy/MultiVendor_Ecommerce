a67e5e48f621a8d803ef1e2a9189ff1b2092cfbf
Maxiflexy <onyemax247@gmail.com>
2025-01-02 17:37:44 +0100

    project set up

---
+++ src/test/java/com/maxiflexy/EcommerceMultiVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiflexy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMultiVendorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
